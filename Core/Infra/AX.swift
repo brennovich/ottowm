fdc3c93580d89da83818a43a6df86426f7c1cdc3
Brenno Costa <brennovich@riseup.net>
2026-08-06 01:18:21 +0200

    Introduce Infra/AX.swift

--- Core/Infra/AX.swift
+++ Core/Infra/AX.swift
@@ -1,7 +1,8 @@
 import ApplicationServices
+import CoreGraphics
 
 // Ceiling on how long a single AX round trip may block the caller.
-let axMessagingTimeoutSeconds: Float = 0.5
+let axMessagingTimeoutSeconds: Float = 0.3
 
 // Undeclared by the AX headers, but answered by the applications we manage.
 let FullScreenAttribute = "AXFullScreen"
@@ -57,4 +58,26 @@ func discardingAXErrors(_ values: [AnyObject]) -> [AnyObject?] {
         if let axValue = axValue(value), AXValueGetType(axValue) == .axError { return nil }
         return value
     }
+}
+
+func decodeCGPoint(_ value: AXValue) -> CGPoint? {
+    guard AXValueGetType(value) == .cgPoint else { return nil }
+    var point = CGPoint.zero
+    return AXValueGetValue(value, .cgPoint, &point) ? point : nil
+}
+
+func decodeCGSize(_ value: AXValue) -> CGSize? {
+    guard AXValueGetType(value) == .cgSize else { return nil }
+    var size = CGSize.zero
+    return AXValueGetValue(value, .cgSize, &size) ? size : nil
+}
+
+func encodeCGPoint(_ point: CGPoint) -> AXValue {
+    var point = point
+    return AXValueCreate(.cgPoint, &point)!
+}
+
+func encodeCGSize(_ size: CGSize) -> AXValue {
+    var size = size
+    return AXValueCreate(.cgSize, &size)!
 }
